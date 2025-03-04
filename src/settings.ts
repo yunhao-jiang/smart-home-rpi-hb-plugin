6138367b8c708fc76962b9ea17120f04861b5049
Yunhao Jiang <39151399+yunhao-jiang@users.noreply.github.com>
2025-03-04 12:26:48 -0800

    feat: initialize plugin & fix homebridge won't restart after config change

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
+export const PLATFORM_NAME = 'Raspberry Pi Smart Home';
 
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-plugin-name';
+export const PLUGIN_NAME = 'homebridge-smart-home-rpi';

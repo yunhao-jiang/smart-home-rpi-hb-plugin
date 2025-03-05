635133fb2d0e6884225faccc2f1a37fd5fac9f37
Yunhao Jiang <39151399+yunhao-jiang@users.noreply.github.com>
2025-03-05 12:08:25 -0800

    feat: connect and expose sensors based on given configuration

--- src/platform.ts
+++ src/platform.ts
@@ -1,6 +1,7 @@
-import type { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
+import { API, Characteristic, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service } from 'homebridge';
+import axios from 'axios';
 
-import { ExamplePlatformAccessory } from './platformAccessory.js';
+// import { ExamplePlatformAccessory } from './platformAccessory.js';
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
 // This is only required when using Custom Services and Characteristics not support by HomeKit
@@ -66,33 +67,66 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
+  async discoverDevices() {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-      {
-        // This is an example of a device which uses a Custom Service
-        exampleUniqueId: 'IJKL',
-        exampleDisplayName: 'Backyard',
-        CustomService: 'AirPressureSensor',
-      },
-    ];
+    
+    // print the config
+    const IP = this.config['IP Address'];
+    const PORT = this.config['Port'];
+    this.log.info('IP:', IP);
+    this.log.info('PORT:', PORT);
+    // const devices = [
+    //   {
+    //     id: 0,
+    //     displayName: 'Temperature Sensor',
+    //     type: this.api.hap.Service.TemperatureSensor,
+    //   },
+    //   {
+    //     id: 1,
+    //     displayName: 'Humidity Sensor',
+    //     type: this.api.hap.Service.HumiditySensor,
+    //   },
+    //   {
+    //     id: 2,
+    //     displayName: 'Motion Sensor',
+    //     type: this.api.hap.Service.MotionSensor,
+    //   },
+    //   {
+    //     id: 3,
+    //     displayName: 'Lightbulb',
+    //     type: this.api.hap.Service.Lightbulb,
+    //   },
+    //   {
+    //     id: 4,
+    //     displayName: 'TV Power',
+    //     type: this.api.hap.Service.Switch
+    //   }
+    // ];
+    // send api request to get the devices
+
+    let devices: { id: number; displayName: string; type: any }[] = [];
+    try{
+      const response = await axios.get(`http://${IP}:${PORT}/api_init`);
+      devices = response.data;
+    }
+    catch (error) {
+      this.log.error('Error:', error);
+    }
+
+    
+    
+    
+    this.log.info('Devices:', devices);
+    
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    for (const device of devices) {
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(String(device.id));
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -100,7 +134,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
       if (existingAccessory) {
         // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        // this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
         // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. e.g.:
         // existingAccessory.context.device = device;
@@ -108,7 +142,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        // new ExamplePlatformAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, e.g.:
         // remove platform accessories when no longer present
@@ -116,18 +150,101 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        
+        // replace - and _ with space
+        device.displayName = device.displayName.replace(/-/g, ' ');
+        device.displayName = device.displayName.replace(/_/g, ' ');
+
+
+        this.log.info('Adding new accessory:', device.displayName);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.displayName, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
+        // accessory.context.device = device;
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        // new ExamplePlatformAccessory(this, accessory); 
+        if (device.type === "bulb") {
+          device.type = this.api.hap.Service.Lightbulb;
+        } else if (device.type === "temp_sensor") {
+          device.type = this.api.hap.Service.TemperatureSensor;
+        }
+        else if (device.type === "humid_sensor") {
+          device.type = this.api.hap.Service.HumiditySensor;
+        }
+        else if (device.type === "motion_sensor") {
+          device.type = this.api.hap.Service.MotionSensor;
+        }
+        else if (device.type === "ir") {
+          device.type = this.api.hap.Service.Switch;
+        }
+
+        const service = accessory.addService(device.type);
+      
+        if (device.type === this.api.hap.Service.Lightbulb) {
+          service.getCharacteristic(this.api.hap.Characteristic.On)
+            .onSet((value) => {
+              this.log.info('Set Characteristic On ->', value);
+              // api request to turn on/off the light
+            })
+            .onGet(() => {
+              // api request to get the status of the light);
+              return true;});
+        } else if (device.type == this.api.hap.Service.TemperatureSensor) {
+          service.getCharacteristic(this.api.hap.Characteristic.CurrentTemperature)
+            .onGet(async () => {
+              try {
+                const response = await axios.post(`http://${IP}:${PORT}/api_post?id=${device.id}`);
+                return response.data.temperature;
+              }
+              catch (error) {
+                this.log.error('Error:', error);
+                return 0;
+              }
+            });
+        } else if (device.type == this.api.hap.Service.HumiditySensor) {
+          service.getCharacteristic(this.api.hap.Characteristic.CurrentRelativeHumidity)
+            .onGet(async () => {
+              try {
+                const response = await axios.post(`http://${IP}:${PORT}/api_post?id=${device.id}`);
+                return response.data.humidity;
+              }
+              catch (error) {
+                this.log.error('Error:', error);
+                return 0;
+              }
+            });
+        } else if (device.type == this.api.hap.Service.MotionSensor) {
+          service.getCharacteristic(this.api.hap.Characteristic.MotionDetected)
+            .onGet(async () => {
+              try {
+                const response = await axios.post(`http://${IP}:${PORT}/api_post?id=${device.id}`);
+                return response.data.motion;
+              }
+              catch (error) {
+                this.log.error('Error:', error);
+                return false;
+              }
+            });
+        } else if (device.type == this.api.hap.Service.Switch) {
+          service.getCharacteristic(this.api.hap.Characteristic.On)
+            .onGet(() => {
+              // get the current state of the switch
+              return true;
+            })
+            .onSet(async (value) => {
+              try {
+                const response = await axios.post(`http://${IP}:${PORT}/api_post?id=${device.id}`);
+              }
+              catch (error) {
+                this.log.error('Error:', error);
+              }
+        });
+        }
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
